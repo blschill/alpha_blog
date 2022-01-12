b19177af64ff3347a8509083b69e5c89f2f0af91
Brady Schiller <blschill@ucsd.edu>
2022-01-12 12:54:13 -0800

    Added Edit and Update Actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,11 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        #byebug
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
         if @article.save
@@ -21,5 +26,15 @@ class ArticlesController < ApplicationController
         else
             render 'new'
        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description)) 
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+        else
+            render "edit"
+        end
     end
 end
