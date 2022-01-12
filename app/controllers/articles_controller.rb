3b01198b81b6ace55033c2c7a8123289c0823eed
Brady Schiller <blschill@ucsd.edu>
2022-01-12 14:20:47 -0800

    Update User Interfacing/Removing Redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,7 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
 
     def show
-        #byebug
-        @article = Article.find(params[:id])
     end
 
     def index
@@ -14,12 +13,10 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        #byebug
-        @article = Article.find(params[:id])
     end
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was created succesfully"
             redirect_to @article
@@ -29,12 +26,26 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description)) 
+        if @article.update(article_params) 
             flash[:notice] = "Article was updated successfully."
             redirect_to @article
         else
             render "edit"
         end
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private #------------ PRIVATE METHODS BELOW THIS LINE
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 end
