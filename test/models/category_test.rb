fa2372042abeb80eb39ca5546711f85c820b65c5
Brady Schiller <blschill@ucsd.edu>
2022-01-20 14:22:53 -0800

    Add Category Validation, RRoutes, Controller, and Views using Controller Tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,9 +2,33 @@ require "test_helper"
 
 class CategoryTest <ActiveSupport::TestCase
 
+  def setup
+    @category = Category.new(name: "Test") 
+  end
+
   test "category should be valid" do
-    @category = Category.new(name: "Test")
     assert @category.valid?
+  end 
+
+  test "name should be present" do
+    @category.name = ""
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    @category.save
+    @new_category = Category.new(name: "Test")
+    assert_not @new_category.valid?
+  end
+
+  test "name should not be too short" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "name should not be too long" do
+    @category.name = "a" * 2
+    assert_not @category.valid?
   end
 
 end
