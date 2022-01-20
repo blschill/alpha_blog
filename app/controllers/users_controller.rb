f83bbcd37f163fe32ce13484453b863dc9a1a395
Brady Schiller <blschill@ucsd.edu>
2022-01-20 12:52:41 -0800

    Create User Destroy Action with Article Destroy Dependency

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :edit]
+  before_action :set_user, only: [:show, :update, :edit, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -38,6 +38,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles were successfully deleted"
+    redirect_to root_path
+  end
 
   private
   def user_params
