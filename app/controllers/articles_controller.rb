b625f987816076f838200c64b8217756696b1579
Brady Schiller <blschill@ucsd.edu>
2022-01-20 16:15:18 -0800

    Add Category Association in the UI

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,8 +18,9 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    byebug
     @article = Article.new(article_params)
-    @article.user = current_user # REMOVE THIS LATER
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was created succesfully"
       redirect_to @article
@@ -49,7 +50,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def require_same_user
